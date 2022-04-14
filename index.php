716e61c094d2e5084cc9f9833add9c253d2c51b0
MarkJastineSiena <sienamarkjastine34@gmail.com>
2022-04-14 18:32:57 +0800

    Info added in index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
             <div class="container px-4">
-                <a class="navbar-brand" href="#page-top"> Add your brand name here </a>
+                <a class="navbar-brand" href="#page-top"> Bass Break</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
@@ -31,7 +31,7 @@
         <header class="bg-dark bg-gradient text-white">
             <div class="container px-4 text-center">
                 <h1 class="fw-bolder">Welcome to Melomaniac</h1>
-                <p class="lead">Add a short description here</p>
+                <p class="lead">Hi There! This page consist of song's lyrics writtenin php script. Fell free to explore and Enjoy.</p>
                 
             </div>
         </header>
@@ -40,16 +40,81 @@
             <div class="container px-4 ">
                 <div class="row gx-4 justify-content-center">
                     <div class="col-lg-12 text-center">
-                        <h2 class="text-primary">Song Title here</h2>
-                        <p class="lead">display the Composer here</p>
-                        <p>display the lyrics/chords here</p>
+                        <h2 class="text-primary">Circles</h2>
+                        <p class="lead">By Post Malone</p>
+                        <?php
+                                echo "<br>";
+                                echo "<p>We couldn't turn around</p>";
+                                echo "<p>'Til we were upside down</p>";
+                                echo "<p>I'll be the bad guy now</p>";
+                                echo "<p>But no, I ain't too proud</p><br>";
+
+                                echo "<p>I couldn't be there</p>";
+                                echo "<p>Even when I tried</p>";
+                                echo "<p>You don't believe it</p>";
+                                echo "<p>We do this every time</p><br>";
+
+                                echo "<p>Seasons change and our love went cold</p>";
+                                echo "<p>Feed the flame 'cause we can't let it go</p>";
+                                echo "<p>Run away, but we're running in circles</p>";
+                                echo "<p>Run away, run away</p><br>";
+
+                                echo "<p>I dare you to do something</p>";
+                                echo "<p>I'm waiting on you again</p>";
+                                echo "<p>So I don't take the blame</p>";
+                                echo "<p>Run away, but we're running in circles</p>";
+                                echo "<p>Run away, run away, run away</p><br>";
+
+                                echo "<p>Let go</p>";
+                                echo "<p>I got a feeling that it's time to let go</p>";
+                                echo "<p>I say so</p>";
+                                echo "<p>I knew that this was doomed from the get-go</p><br>";
+
+                                echo "<p>You thought that it was special, special</p>";
+                                echo "<p>But it was just the sex though, the sex though</p>";
+                                echo "<p>And I still hear the echoes (the echoes)</p>";
+                                echo "<p>I got a feeling that it's time to let it go, let it go</p><br>";
+
+                                echo "<p>Seasons change and our love went cold</p>";
+                                echo "<p>Feed the flame 'cause we can't let it go</p>";
+                                echo "<p>Run away, but we're running in circles</p>";
+                                echo "<p>Run away, run away</p><br>";
+
+                                echo "<p>I dare you to do something</p>";
+                                echo "<p>I'm waiting on you again</p>";
+                                echo "<p>So I don't take the blame</p>";
+                                echo "<p>Run away, but we're running in circles</p>";
+                                echo "<p>Run away, run away, run away</p><br>";
+
+
+                                echo "<p>Maybe you don't understand what I'm going through</p>";
+                                echo "<p>It's only me</p>";
+                                echo "<p>What you got to lose?</p>";
+                                echo "<p>Make up your mind, tell me</p>";
+                                echo "<p>What are you gonna do?</p>";
+                                echo "<p>It's only me</p>";
+                                echo "<p>Let it go</p><br>";
+
+                                echo "<p>Seasons change and our love went cold</p>";
+                                echo "<p>Feed the flame 'cause we can't let it go</p>";
+                                echo "<p>Run away, but we're running in circles</p>";
+                                echo "<p>Run away, run away</p><br>";
+
+                                echo "<p>I dare you to do something</p>";
+                                echo "<p>I'm waiting on you again</p>";
+                                echo "<p>So I don't take the blame</p>";
+                                echo "<p>Run away, but we're running in circles</p>";
+                                echo "<p>Run away, run away, run away</p><br>";
+
+                                echo "<p>Songwriters: <i>Louis Bell / Adam King Feeney / William Walsh / Austin Post / Kaan Gunesberk</i></p>";
+                         ?>
                     </div>
                 </div>
             </div>
         </section>
 
         <footer class="py-3 bg-danger">
-            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. add your fullname here</p></div>
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Mark Jastine D. Siena</p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="js/bootstrap.bundle.min.js"></script>
